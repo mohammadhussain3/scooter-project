9025bdb4b581ca604ebd656033cfc0117410c0cb
mohammadhussain3 <mhussain3@westminster.gov.uk>
2023-02-09 12:35:44 +0000

    created a UML diagrm and constructors for JS Class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,12 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  constructor(station, user, serial, nextSerial, charge, isBroken) {
+    this.station = station;
+    this.user = user;
+    this.serial = serial;
+    this.nextSerial = nextSerial
+    this.charge = charge;
+    this.isBroken = isBroken
+  }
 }
 
 
--- src/User.js
+++ src/User.js
@@ -1,5 +1,11 @@
+
 class User {
-  // User code here
+  constructor(username, password, age, loggedIn) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = loggedIn;
+  }
 }
 
 module.exports = User
